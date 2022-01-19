a5029fed54608325b044adf6dc2f525500ea82a9
Vlad Boyko <swampman@inbox.ru>
2022-01-19 19:44:08 +0300

    [add] keyboard

--- main.go
+++ main.go
@@ -8,16 +8,30 @@ import (
 	godotenv "github.com/joho/godotenv"
 )
 
-var numericKeyboard = tgbotapi.NewReplyKeyboard(
-	tgbotapi.NewKeyboardButtonRow(
-		tgbotapi.NewKeyboardButton("1"),
-		tgbotapi.NewKeyboardButton("2"),
-		tgbotapi.NewKeyboardButton("3"),
+var cntAll, cntYes, cntNo int
+var ages = [6]string{"до 20", "20-29", "30-39", "40-49", "50-59", "60 ++"}
+var ages_stat = [6][2]int{{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}}
+var ageGroup = map[string]int{ages[0]: 15, ages[1]: 25, ages[2]: 35, ages[3]: 45, ages[4]: 55, ages[5]: 65}
+var user_age = make(map[int64]int)
+var user_timestamp = make(map[int64]int64)
+var repeat_msg = "\nДля повторного показа статистики введите любой текст или нажмите Start, но не ранее чем через 10 секунд"
+
+var numericInlineKeyboard = tgbotapi.NewInlineKeyboardMarkup(
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData(ages[0], ages[0]),
+		tgbotapi.NewInlineKeyboardButtonData(ages[1], ages[1]),
+		tgbotapi.NewInlineKeyboardButtonData(ages[2], ages[2]),
 	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData(ages[3], ages[3]),
+		tgbotapi.NewInlineKeyboardButtonData(ages[4], ages[4]),
+		tgbotapi.NewInlineKeyboardButtonData(ages[5], ages[5]),
+	),
+)
+
+var startKeyboard = tgbotapi.NewReplyKeyboard(
 	tgbotapi.NewKeyboardButtonRow(
-		tgbotapi.NewKeyboardButton("4"),
-		tgbotapi.NewKeyboardButton("5"),
-		tgbotapi.NewKeyboardButton("6"),
+		tgbotapi.NewKeyboardButton("Start"),
 	),
 )
 
@@ -26,6 +40,8 @@ func main() {
 		log.Print("No .env file found")
 	}
 	API_TOKEN := os.Getenv("TELEGRAM_API_TOKEN")
+
+	cntAll, cntYes, cntNo = 0, 0, 0
 
 	bot, err := tgbotapi.NewBotAPI(API_TOKEN)
 	if err != nil {
@@ -40,19 +56,36 @@ func main() {
 	u.Timeout = 60
 
 	updates := bot.GetUpdatesChan(u)
+	var userID int64
+	var userData string
 
 	for update := range updates {
-		if update.Message == nil { // ignore non-Message updates
+		if update.Message == nil && update.CallbackQuery == nil { // ignore non-Message updates
 			continue
 		}
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		userID = update.Message.From.ID
+		if update.CallbackQuery != nil {
+			userData = update.CallbackQuery.Data
+		}
+		log.Printf("userID %d, %s", userID, userData)
+		if tout, ok := user_timestamp[userID]; ok {
+			log.Printf("Found timestamp %d", tout)
+			user_timestamp[userID] = tout
+		} else {
+			log.Printf("Init timestamp 0")
+			user_timestamp[userID] = 10
+		}
 
-		switch update.Message.Text {
-		case "open":
-			msg.ReplyMarkup = numericKeyboard
-		case "close":
-			msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+		// new user - send age question
+		user_age[userID] = 0
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		log.Printf("Message text: %s", msg.Text)
+		if update.Message.Text == "Start" {
+			msg.ReplyMarkup = numericInlineKeyboard
+		} else {
+			msg.ReplyMarkup = startKeyboard
 		}
 
 		if _, err := bot.Send(msg); err != nil {
